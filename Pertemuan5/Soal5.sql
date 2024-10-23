91e5c313ebc869610701bb9f623caa99bf55ecef
Diana-museo <emeraldadiana2425@gmail.com>
2024-10-23 10:28:09 +0700

    Modify

--- Pertemuan5/Soal5.sql
+++ Pertemuan5/Soal5.sql
@@ -1,6 +1,6 @@
 Use Pertemuan5;
-
-INSERT INTO Dosen_Wali(id, NIP, nama, tanggal_lahir, alamt, no_hp)
+s
+INSERT INTO Dosen_Wali (id, NIP, nama, tanggal_lahir, alamt, no_hp)
 VALUES (1, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266),
 (2, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266),
 (3, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266),
@@ -10,4 +10,4 @@ VALUES (1, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266),
 (7, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266),
 (8, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266),
 (9, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266),
-(10, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266);
+(10, 001, 'Asep', '1980-01-22', 'Jl. Rambutan', 081266),
